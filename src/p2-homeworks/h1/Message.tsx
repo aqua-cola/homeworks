758000f0041dcf9df0b98ff1010f65e78f7a5dd6
Diyar Mashrapov <mashrapovd@gmail.com>
2021-11-24 12:13:21 +0600

    5 dz

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.avatar}><img src={props.avatar} alt="404"/></div>
+            <div className={s.ugol}></div>
+            <div className={s.text}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.messageText}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
@@ -13,14 +13,18 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error ? s.error : '' // need to fix with (?:)
+    const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter')
+            addUser()
+    }
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
+        <div className={s.someClass}>
+            <input value={name} onChange={setNameCallback} onKeyPress={onKeyPress} className={inputClass}/>
             <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <span className={s.totalUsers}>{totalUsers}</span>
+            <div className={s.red}>{error && 'PLEASE, ENTER THE NAME!'}</div>
         </div>
     )
 }
